3ba44ab93cf4e2e75749636e09cc4312d25c4de5
Fabiana1vo <fabianasouzaivo@icloud.com>
2023-07-23 16:59:12 -0300

    Validador de CPF finalizado

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
-// let cpf = '705.484.450-52'
-// let cpfLimpo = cpf.replace(/\D+/g, '');
-
-// Array.from faz com que eu crie um novo array com algum item
-
-
 // Função construtora
-
 function ValidaCPF(cpfEnviado) {
     Object.defineProperty(this, 'cpfLimpo', {
-        get: function(){
+        get: function () {
             return cpfEnviado.replace(/\D+/g, '');
         }
     });
 }
 
-
-// Método responsavel por validar o cpf
-ValidaCPF.prototype.valida = function(){
-    if(typeof this.cpfLimpo === 'undefined')  return false;
-    if(this.cpfLimpo.length !== 11) return false;    
+// Método responsavel por executar a validação de um CPF
+ValidaCPF.prototype.valida = function () {
+    if (typeof this.cpfLimpo === 'undefined') return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
 
-    return true;
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+};
+
+
+// Método responsável por realizar a validação de um CPF de fato
+ValidaCPF.prototype.criaDigito = function (cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0)
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
 }
 
-ValidaCPF.prototype.criaDigito = function(cpfParcial){
-const cpfArray = Array.from(cpfParcial);
-console.log(cpfArray);
+// Método responsável por verificar se o número do CPF é uma sequencia Ex: (111.111.111-11) - E invalidar o CPF
+ValidaCPF.prototype.isSequencia = function(){
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
 }
 
-
-
+// Váriavel que passa um CPF para que seja realizado o teste.
 const cpf = new ValidaCPF('705.484.450-52');
-
-console.log(cpf.cpfLimpo)
-console.log(cpf.valida());
+ 
